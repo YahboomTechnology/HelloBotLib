7ac30a12f1bc3d86bc7f38362ce778fed722a1eb
Yahboom Technology <35594203+YahboomTechnology@users.noreply.github.com>
2021-05-13 15:29:25 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (C): 2010-2019, Shenzhen Yahboom Tech
 modified from liusen
 load dependency
@@ -622,6 +622,31 @@ namespace HelloBot {
 		
 		        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High, 43200);
 		        list[i] = Math.floor(d / 40)
+        }
+        list.sort();
+        let length = (list[1] + list[2] + list[3])/3;
+        return  Math.floor(length);
+    }
+        
+		//% blockId=HelloBot_ultrasonic_carV2 block="ultrasonic for V2 return distance(cm)"
+    //% color="#006400"
+    //% weight=88
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Ultrasonic_CarV2(): number {
+
+        // send pulse       
+        let list:Array<number> = [0, 0, 0, 0, 0];
+        for (let i = 0; i < 5; i++) {
+            pins.setPull(DigitalPin.P14, PinPullMode.PullNone);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		        control.waitMicros(2);
+		        pins.digitalWritePin(DigitalPin.P14, 1);
+		        control.waitMicros(15);
+		        pins.digitalWritePin(DigitalPin.P14, 0);
+		
+		        let d = pins.pulseIn(DigitalPin.P15, PulseValue.High);
+		        list[i] = Math.floor(d / 58)
         }
         list.sort();
         let length = (list[1] + list[2] + list[3])/3;
